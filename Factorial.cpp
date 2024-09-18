b9287f99d0f844591ada14a3866a7d1eb3b00f38
Michael J <30031129@cc.peralta.edu>
2024-09-17 18:16:14 -0700

    calculated factorial

--- Factorial.cpp
+++ Factorial.cpp
@@ -9,6 +9,11 @@ int main(){
     cout << "Please type out a positive number" << endl;
     cin >> number;
 
-    
+    for(int i = number - 1; i > 0; i--){
+        factorial = factorial * i;
+    }
+   
+    cout << "Your number factorial is: " << factorial;
+
     return 0;
 }
