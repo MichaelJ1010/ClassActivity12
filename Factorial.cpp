72ee7958dde05c139c758729aea849b85f461fba
Michael J <30031129@cc.peralta.edu>
2024-09-17 18:15:13 -0700

    storing number

---
+++ Factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number;
+    int factorial;
+
+    cout << "Please type out a positive number" << endl;
+    cin >> number;
+
+    
+    return 0;
+}
